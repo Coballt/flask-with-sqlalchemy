72d866707f7e3a6934e572f04d207ef50a113d02
Thibaut DELAROZIERE <thibaut.delaroziere@gmail.com>
2018-07-12 10:59:34 +0200

    CRUD deployment to heroku

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,8 @@ try:
     load_dotenv()
 except:
     pass # Heroku does not use .env
-
-from flask import Flask
+import json
+from flask import Flask, jsonify, request
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from models import Product
-from schemas import products_schema
+from schemas import products_schema, product_schema
 
 @app.route('/')
 def hello():
@@ -25,3 +25,52 @@ def hello():
 def products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
+
+@app.route('/api/v1/products/<int:id>')
+def get_products(id):
+    products = db.session.query(Product).get(id)
+    if products is not None :
+        return product_schema.jsonify(products)
+    return jsonify({"error": "Product not found"}), 404
+
+@app.route('/api/v1/products', methods=['POST'])
+def post_products():
+    try:
+        payload = json.loads(request.data)
+    except ValueError :
+        return jsonify({"error" : "Bad payload received"}), 422
+    if 'name' not in payload or 'description' not in payload:
+        return jsonify({"error" : "Bad payload received"}), 422
+    product = Product(name=payload['name'], description=payload['description'])
+    db.session.add(product)
+    db.session.flush()
+    db.session.commit()
+    return product_schema.jsonify(product), 201
+
+@app.route('/api/v1/products/<int:id>', methods=['DELETE'])
+def delete_products(id):
+    product = db.session.query(Product).get(id)
+    if product is not None :
+        db.session.delete(product)
+        db.session.flush()
+        db.session.commit()
+        return '', 204
+    return jsonify({"error": "Product not found"}), 404
+
+@app.route('/api/v1/products/<int:id>', methods=['PATCH'])
+def update_products(id):
+    try:
+        payload = json.loads(request.data)
+    except ValueError :
+        return jsonify({"error" : "Bad payload received"}), 422
+    if 'name' not in payload and 'description' not in payload:
+        return jsonify({"error" : "Bad payload received"}), 422
+    product = db.session.query(Product).get(id)
+    if product is not None :
+        if 'name' in payload:
+            product.name = payload['name']
+        if 'description' in payload:
+            product.description = payload['description']
+        db.session.commit()
+        return '', 204
+    return jsonify({"error": "Product not found"}), 404
